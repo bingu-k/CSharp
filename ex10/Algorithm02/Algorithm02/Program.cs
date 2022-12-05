0d39f79dc877e5233b6714d50fa0a5dc8b1a2435
byeunghyeun kim <bingu4761@naver.com>
2022-12-05 16:11:57 +0900

    Success Maze Pre-set

---
+++ ex10/Algorithm02/Algorithm02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.Initialize(25);
+
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTick = 0;
+            while (true)
+            {
+                #region 프레임관리
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                #endregion
+                // 입력
+                // 로직
+                // 랜더링
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+    }
+}
